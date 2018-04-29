e40b302e6c024f8fe65f3b3e25148a5120b43a2b
Yana <yansy.blue.1998@gmail.com>
2018-04-29 14:48:34 +0300

    chore

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { addTodoBlock } from "../actions/todoBlocks";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 const Dashboard = ({ closedTodoBlocks, dispatch }) => (
     <h1
